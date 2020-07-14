0fe92fee42b30569111a9f05e301fd20ae467e64
Jill Short <jillian.b.short@gmail.com>
2020-07-14 20:24:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,7 @@ def assign_rooms (attendees)
 end
 
 def printer (attendees)
-  batch_badge_creator (attendees).each do |badge|
+  batch_badge_creator(attendees).each do |badge|
     puts badge
   end
 end
