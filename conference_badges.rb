aaa9a77dfd90633aaf730892b24dc8284d3ec445
Jill Short <jillian.b.short@gmail.com>
2020-07-14 17:32:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,5 +13,5 @@ end
 def assign_rooms (attendees)
   room_assignments = []
   attendees.each_with_index do |attendee|
-    room_assignments <<
+    room_assignments << "Hello, #{attendees}! You'll be assigned to room "
 end
