f6eefbe9e03ea7ec3908d0a2a34768e8f3f961c8
Jill Short <jillian.b.short@gmail.com>
2020-07-14 16:45:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,7 @@ def batch_badge_creator (badges)
   badges = []
   attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   attendees.each do |attendee|
-    puts "Hello, my name is #{attendee}."
+    badges << "Hello, my name is #{attendee}."
   end
   
 end
