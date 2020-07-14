a54f505af3fad4fb58969d809c20ef075f76051b
Jill Short <jillian.b.short@gmail.com>
2020-07-14 18:01:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -31,6 +31,6 @@ def printer (attendees)
     room_assignments << "Hello, #{attendee}! You'll be assigned to room #{counter}!"
     counter += 1 
   end
-  badge = gets.chomp
-  room_assignments = gets.chomp
+  puts badge
+  puts room_assignments
 end
